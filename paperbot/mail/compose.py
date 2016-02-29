f45b01a1bea8a37a299134a3953fa1a9009cf234
Vinícius Pinto Menézio <v.menezio@gmail.com>
2016-02-29 10:34:46 -0300

    Changes to the compose module.
    Separated data files from code, rewrote some of the documentation for
    the compose module and made sure the generated html is properly encoded.

--- paperbot/mail/compose.py
+++ paperbot/mail/compose.py
@@ -7,15 +7,15 @@ import mistune
 import cssutils
 from collections import namedtuple
 
-DIR = os.path.dirname(__file__)+"/"
+DATA = os.path.dirname(__file__)+"/data/"
 
-def create_newsletter(urls, base_md=DIR+"base.md", subs_json=DIR+"subs.json",
-        links_json=DIR+"links.json", style_css=DIR+"style.css"):
+def create_newsletter(urls, base_md=DATA+"base.md", subs_json=DATA+"subs.json",
+        links_json=DATA+"links.json", style_css=DATA+"style.css"):
     """Return a newsletter object.
 
-    The newsletter is built from a list of URLs, a base file (markdown)
-    and a substituitions file, creating a somewhat unique message each
-    time. It is then returned as a namedtuple Newsletter(html, text).
+    Build a newsletter from a list of URLs, a base file (markdown) and a
+    substituitions file, creating a somewhat unique message each time;
+    then return it as a namedtuple Newsletter(html_text, plain_text).
     """
     substitutions = get_substitutions(subs_json)
     date = arrow.now().format("DD/MM/YYYY")
@@ -27,20 +27,22 @@ def create_newsletter(urls, base_md=DIR+"base.md", subs_json=DIR+"subs.json",
         markdown = base_file.read().format(**substitutions, date=date_weekday,
             user_links=user_links, paperbot_links=paperbot_links)
 
-    html = style(mistune.markdown(markdown), style_css)
-    text = html_to_text(html)
+    html_text = style_html(mistune.markdown(markdown), style_css)
+    encoded_html_text = html_text.encode("latin-1", "xmlcharrefreplace")
+    plain_text = html_to_text(html_text)
 
-    return namedtuple("Newsletter", ["html","text","date"])(html, text, date)
+    Newsletter = namedtuple("Newsletter", ["html_text","plain_text","date"])
+    return Newsletter(encoded_html_text, plain_text, date)
 
-def html_to_text(html):
-    """Strip html elements from string and return a plaintext version."""
+def html_to_text(html_text):
+    """Strip html_text tags from string and return a plain plain_text version."""
     strip_tags = regex.compile(r"<.*?>")
     eol_tags = regex.compile(r"</p>")
-    return strip_tags.sub("", eol_tags.sub("\n", html))
+    return strip_tags.sub("", eol_tags.sub("\n", html_text))
 
-def style(html, style_css):
-    """Applies css styles from an external file to an html string inline."""
-    styled_html = html
+def style_html(html_text, style_css):
+    """Applies inline styles to an html_text string from an externa file."""
+    styled_html = html_text
 
     with open(style_css) as css_file:
         css = cssutils.parseString(css_file.read())
@@ -65,22 +67,22 @@ def style(html, style_css):
     return styled_html
 
 def get_substitutions(subs_json):
-    """Randomly pull word/phrase variations from the substitutions file."""
+    """Randomly pull phrase variations from the substitutions file."""
     with open(subs_json) as subs_file:
         raw_subs = json.loads(subs_file.read())
     return { k : random.choice(raw_subs[k]) for k in raw_subs }
 
 def parse_links(links_json): # TODO: paperbot's suggestions
-    """Return a html string with lists made up from the links file."""
+    """Return a html_text string with lists made up from the links file."""
     with open(links_json) as links_file:
         raw_links = json.loads(links_file.read())
     user_links = raw_links["user_links"]
     paperbot_links = raw_links["paperbot_links"]
-    return get_html_links(user_links), "paperbot's suggestions coming soon"
+    return get_md_links(user_links), "paperbot's suggestions coming soon"
 
-def get_html_links(link_list):
+def get_md_links(link_list):
+    """Return an unordered list of links in markdown syntax."""
     html_links = "\n"
     for link in link_list:
         html_links += "* [{title}]({url})\n".format(**link)
     return html_links
-
